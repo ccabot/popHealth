e15136eba17bd7be0088fae281924197bb8b185f
toby cabot <ccabot@mitre.org>
2010-07-20 10:54:12 -0400

    deleted the UserObserver class
    i'll need to figure out how MongoMapper observers work at some point
    but for now this is the only one so i'm just going to blow it away.

--- config/environment.rb
+++ config/environment.rb
@@ -58,7 +58,9 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
-  config.active_record.observers = :user_observer
+# FIXME I'll need to figure out how Observers in MongoMapper work but in the mean
+# time I'm going to disable this
+#  config.active_record.observers = :user_observer
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
